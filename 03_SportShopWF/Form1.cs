e6f9e222746d5cbbbd095581db2e596fe9e9d434
ilusin_o@itstep.academy <ilusin_o@itstep.academy>
2023-02-11 13:20:20 +0200

    Create library data_access

---
+++ 03_SportShopWF/Form1.cs
@@ -0,0 +1,31 @@
+﻿using _04_data_access;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _03_SportShopWF
+{
+    public partial class Form1 : Form
+    {
+        string conn = @"Data Source=DESKTOP-3HG9UVT\SQLEXPRESS;
+                            Initial Catalog=SportShop;
+                            Integrated Security=True;
+                            Connect Timeout=2;";
+        public Form1()
+        {
+            SportShopDb db = new SportShopDb(conn);
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Ite
+        }
+    }
+}
