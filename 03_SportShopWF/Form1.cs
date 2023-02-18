8d55295386676f62fe23628bd4f54efc214c44f6
ilusin_o@itstep.academy <ilusin_o@itstep.academy>
2023-02-18 13:21:25 +0200

    DisconnectedMode

--- 03_SportShopWF/Form1.cs
+++ 03_SportShopWF/Form1.cs
@@ -2,7 +2,9 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -13,21 +15,55 @@ namespace _03_SportShopWF
 {
     public partial class Form1 : Form
     {
-        string conn = @"Data Source=DESKTOP-3HG9UVT\SQLEXPRESS;
-                            Initial Catalog=SportShop;
-                            Integrated Security=True;
-                            Connect Timeout=2;";
-        SportShopDb db;
+       
+        //SportShopDb db;
+        private SqlConnection con = null;
+        private SqlDataAdapter da = null;
+        private DataSet set = null;
         public Form1()
         {
-             db = new SportShopDb(conn);
+            //db = new SportShopDb(conn);
             InitializeComponent();
             dataGridView1.Size = new Size(2600, 2600);
+            string cs = ConfigurationManager.ConnectionStrings["SportShopDb"].ConnectionString;
+            con = new SqlConnection(cs);
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            dataGridView1.DataSource = db.GetAll();
+            //dataGridView1.DataSource = db.GetAll();
+            //Fill
+            try
+            {
+                string sql = textBox1.Text;
+                da = new SqlDataAdapter(sql, con);
+                SqlCommandBuilder cmd = new SqlCommandBuilder(da);
+
+                set = new DataSet();
+                da.Fill(set,"MyTable");
+
+                dataGridView1.DataSource = null;
+                dataGridView1.DataSource = set.Tables["MyTable"];
+
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private void UpdateBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                da.Update(set, "MyTable");
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
