11dddf774eaab0c753849ae4921d9890ca9c4c71
ilusin_o@itstep.academy <ilusin_o@itstep.academy>
2023-03-11 13:24:41 +0200

    Intro to Entity Framework

---
+++ 06_IntroToEntityFramework/Program.cs
@@ -0,0 +1,28 @@
+﻿using _06_IntroToEntityFramework;
+using System;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace _01_Ado_Net
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AirplaneDbContext context = new AirplaneDbContext();
+            context.Clients.Add(new Client
+            {
+                Name = "Volodia",
+                Birthdate = new DateTime(2006, 7, 9),
+                Email = "volodia@gmail.com"
+            });
+            context.SaveChanges();
+
+            foreach (var c in context.Clients)
+            {
+                Console.WriteLine($"Client : {c.Name}  {c.Email}  {c.Birthdate.ToShortDateString()}");
+            }
+
+        }
+    }
+}
